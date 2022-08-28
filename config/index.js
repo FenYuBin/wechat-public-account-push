32854bbcdff7f9e25320d52bdb12379068ca7cfc
FenYuBin <61956123+FenYuBin@users.noreply.github.com>
2022-08-29 00:30:09 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -8,9 +8,9 @@ export const config = {
     // 公众号APP_SECRET
     APP_SECRET: "bca53ae3c198cc9c91fa85f721c72500",
     // 模板消息id
-    TEMPLATE_ID: "FaMIxsgG7qWzZb-Ahwvs7LD0571JEk0KCXMKYrCMgGQ",
+    TEMPLATE_ID: "V6ACI2AJf9KyhlTzLTspubPiLWCBrzl7nYkXtoL4L1M",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    USERS: ["oRsQE6RoLmii_kGTdQwLmbNweLeM"],
+    USERS: ["oRsQE6RoLmii_kGTdQwLmbNweLeM"，"oRsQE6dnx5v_DFMH-9OJJodOvQ2s"],
      
     /**
      * 信息配置
